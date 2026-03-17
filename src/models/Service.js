3d4aac251adfe4c99053fe1881b32031717f455f
ErvinChavez <ervinchavez31@gmail.com>
2026-03-17 01:28:35 -0400

    Added Service.js file

---
+++ src/models/Service.js
@@ -0,0 +1,10 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Service = sequelize.define('Service', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.TEXT, allowNull: true },
+}, { timestamps: true });
+
+module.exports = Service;
