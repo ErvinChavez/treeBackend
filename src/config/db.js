682d05dff028b8d40994ae6f71982f4b0cc41620
ErvinChavez <ervinchavez31@gmail.com>
2026-05-03 12:06:52 -0400

    connecting DB to supabase

--- src/config/db.js
+++ src/config/db.js
@@ -3,13 +3,15 @@ require('dotenv').config(); //Load .env
 
 //Connect to PostgreSQL
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
+    process.env.DATABASE_URL, {
         dialect: 'postgres',
-        logging: console.log, //Set to false when deployment.
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            }
+        }
     }
 );
 
