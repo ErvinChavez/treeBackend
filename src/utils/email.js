826db7c6079c08bc0c6531d27b4eb1bdc6f9f9a0
ErvinChavez <ervinchavez31@gmail.com>
2026-05-07 22:49:13 -0400

    decoding email error

--- src/utils/email.js
+++ src/utils/email.js
@@ -7,59 +7,26 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: EMAIL_USER,
     pass: EMAIL_PASS,
   },
 });
 
-//error finding
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("Email transporter failed:", error);
-  } else {
-    console.log("Email transporter ready");
-  }
-});
-
 async function sendEmail(to, subject, html) {
   try {
-    console.log("EMAIL ATTEMPT:", {
-      to,
-      user: process.env.EMAIL_USER,
-    });
-
-    const info = await transporter.sendMail({
-      from: `"Chavez Tree Service" <${EMAIL_USER}>`,
+    await transporter.sendMail({
+      from: EMAIL_USER,
       to,
       subject,
       html,
     });
 
-    console.log("STEP 2: after sendMAil", info);
-
-    return info;
-
   } catch (err) {
-    console.error("EMAIL FULL ERROR:");
-    console.error(err); // IMPORTANT (not just message)
+    console.error("Email error:", err);
     throw err;
   }
 }
-
-// async function sendEmail(to, subject, html) {
-//   try {
-//     await transporter.sendMail({
-//       from: EMAIL_USER,
-//       to,
-//       subject,
-//       html,
-//     });
-
-//   } catch (err) {
-//     console.error("Email error:", err);
-//     throw err;
-//   }
-// }
 
 module.exports = sendEmail;
